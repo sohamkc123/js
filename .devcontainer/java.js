036ee9b38bb8b14f25b35e4c377f480279ae81fa
sohamkc123 <134222701+sohamkc123@users.noreply.github.com>
2023-10-02 10:17:47 +0000

    yai yo katha

--- .devcontainer/java.js
+++ .devcontainer/java.js
@@ -3,6 +3,7 @@ console.log("hello world")
 let name="soham"//let value can be change in middle of the program
 var adress="deepsana"
 const pi=3.18
+let score=null
 
 // printing multiple things in table
 console.table([name , adress])
@@ -12,3 +13,14 @@ console.log(name);
 console.log(typeof "soham")
  console.log(typeof null);
 console.log(typeof undefined);
+
+// change to other datatype,conversion
+let sarthak= Number(adress) 
+console.log(typeof sarthak);
+let s=69
+let d=-s
+console.log(d)
+
+// some amazing things in js;check the results
+console.log("1"+2+2)
+console.log(1+2+"2");
