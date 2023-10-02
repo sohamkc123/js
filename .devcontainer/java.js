e54bdb9150ef3d4b1fa5028f0e61c82d084e1f28
sohamkc123 <134222701+sohamkc123@users.noreply.github.com>
2023-10-02 09:42:59 +0000

    done

--- .devcontainer/java.js
+++ .devcontainer/java.js
@@ -1,1 +1,14 @@
 console.log("hello world")
+// let var and const
+let name="soham"//let value can be change in middle of the program
+var adress="deepsana"
+const pi=3.18
+
+// printing multiple things in table
+console.table([name , adress])
+console.log(name);
+
+// how can we check the datatype
+console.log(typeof "soham")
+ console.log(typeof null);
+console.log(typeof undefined);
